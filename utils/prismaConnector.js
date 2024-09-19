24c8ff1002d9ff3cfac136b1e42f093e72bf8080
Adrian T <43932476+Ssmidge@users.noreply.github.com>
2024-09-19 17:39:20 -0400

    Removed default from getPrisma function as it is not needed

--- utils/prismaConnector.js
+++ utils/prismaConnector.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 let prismaClient;
 
-export default function getPrisma() {
+export function getPrisma() {
     if (!prismaClient) prismaClient = new PrismaClient();
     return prismaClient;
 }
