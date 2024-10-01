82df292732cefdc02b920c4998771ffd9edbfb53
Arav Narula <arav.1tb@gmail.com>
2024-09-30 20:43:36 -0400

    Revert "Revert "Updated command stuff and fixed the prismaConnector""

--- utils/prismaConnector.js
+++ utils/prismaConnector.js
@@ -1,7 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
 let prismaClient;
 
-export function getPrisma() {
+function getPrisma() {
     if (!prismaClient) prismaClient = new PrismaClient();
     return prismaClient;
 }
+
+module.exports = { getPrisma };
