b95714b64b812e38693eb92fd8bb49e675a2b722
Arav Narula <arav.1tb@gmail.com>
2024-10-01 20:05:18 -0400

    unbanning works

--- commands/unban.js
+++ commands/unban.js
@@ -5,24 +5,28 @@ async function unban(args) {
     const { user_id, text, channel_id } = payload;
     const prisma = getPrisma();
     const commands = text.split(" ");
-    const userToBan = commands[1].split('|')[0].replace("<@", "");
-    let channel = commands[0].split('|')[0].replace("<#", "");
-    const updateUser = await prisma.user.deleteMany({ where: { user: userToBan, channel: channel } });
+    const userToBan = commands[0].split('|')[0].replace("<@", "");
+    let channel = commands[1].split('|')[0].replace("<#", "");
+    const getUser = await prisma.user.deleteMany({ where: { user: userToBan, channel: channel } });
+
 
     if (!userToBan || !channel || !userToBan && !channel) { 
       await client.chat.postEphemeral({
         channel: `${channel_id}`,
         user: `${user_id}`,
         text: "Invaild arugements"
-      });
+      })
+    }
+    else {
+      const updateUser = await prisma.user.deleteMany({ where: { user: userToBan, channel: channel } });
     }
     await client.chat.postMessage({
       channel: userToBan,
-      text: `You were unbanned from ${updateUser.channel}`
+      text: `You were unbanned from ${channel}`
     });
-    await client.chat.postEphemeral({
+    await client.chat.postMessage({
       channel: process.env.MIRRORCHANNEL,
-      text: `<@${userToBan}> was unbanned from ${updateUser.channel}`
+      text: `<@${userToBan}> was unbanned from ${channelnnel}`
     });
 }
 
