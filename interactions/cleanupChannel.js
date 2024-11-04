5fbb708cd5fa10353039027ab36cddffa791c646
Arav Narula <arav.1tb@gmail.com>
2024-11-04 15:05:43 -0500

    add admin bypass

--- interactions/cleanupChannel.js
+++ interactions/cleanupChannel.js
@@ -7,8 +7,12 @@ async function cleanupChannel(args) {
     const { client, payload } = args
     const { user, ts, thread_ts, text, channel, subtype, bot_id } = payload
     const prisma = getPrisma();
+    const userInfo = await client.users.info({ user: user });
+    const isAdmin = (await userInfo).user.is_admin;
 
 
+
+    if (isAdmin) return;
 
     const getChannel = await prisma.Channel.findFirst({
         where: {
