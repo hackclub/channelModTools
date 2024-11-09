94295a51dc8c9775844e661d946c5744b4a7b6aa
Arav Narula <arav.1tb@gmail.com>
2024-11-09 16:19:35 -0500

    add code that checks if someone is a channel manger from David's code

---
+++ utils/isChannelManger.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+module.exports = async function getChannelManagers(channel) {
+  const myHeaders = new Headers();
+  myHeaders.append("Cookie", `d=${process.env.SLACK_USER_COOKIE}`);
+
+  const formdata = new FormData();
+  formdata.append("token", process.env.SLACK_BROWSER_TOKEN);
+  formdata.append("entity_id", channel);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formdata,
+    redirect: "follow",
+  };
+
+  const request = await fetch(
+    "https://slack.com/api/admin.roles.entity.listAssignments",
+    requestOptions,
+  );
+
+  const json = await request.json();
+
+  if (!json.ok) return [];
+  return json.role_assignments[0]?.users || [];
+};
