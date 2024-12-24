3d7c3181b40bc73d154fed593877a4760f911f8c
Arav Narula <arav.1tb@gmail.com>
2024-12-23 21:38:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
+    // socketMode: true,
+    // appToken: process.env.SLACK_APP_TOKEN,
     // Using socket mode, however we still want for it to reply to OAuth
     port: process.env.PORT || 3000,
 });
