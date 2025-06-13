48241f4f5b0a8e86e615fa9ac86e73ad4e038d17
Hussein <143280315+46-HA@users.noreply.github.com>
2025-06-13 01:02:37 -0500

    added autoMod.js

--- index.js
+++ index.js
@@ -41,20 +41,24 @@ app.event("channel_created", async ({ event, client }) => {
 
 app.event('message', async (args) => {
     // begin the firehose
-    const { body, client } = args
-    const { event } = body
-    const { type, subtype, user, channel, ts, text } = event
+    const { body, client } = args;
+    const { event } = body;
+    const { type, subtype, user, channel, ts, text } = event;
 
     const cleanupChannel = await require("./interactions/cleanupChannel.js");
     await cleanupChannel(args);
+
     const shushBan = await require("./interactions/listenforBannedUser.js");
     await shushBan(args);
+
     const startSlowMode = await require("./interactions/startSlowMode.js");
     await startSlowMode(args);
+
     const listenforChannelBannedUser = await require("./interactions/listenforChannelBannedUser.js");
     await listenforChannelBannedUser(args);
 
-
+    const autoMod = await require("./interactions/autoMod.js");
+    await autoMod(args);
 });
 
 const handleEvent = require("./events/index.js");
