c324b49d3ba0cba3b49e826f71ba971f1d8d7320
Arav Narula <arav.1tb@gmail.com>
2025-09-02 14:38:20 -0400

    remove self shush

--- commands/unshush.js
+++ commands/unshush.js
@@ -6,26 +6,35 @@ async function unshush(args) {
   const { user_id, text, channel_id } = payload;
   const prisma = getPrisma();
   const commands = text.split(" ");
+  const userInfo = await client.users.info({ user: user_id });
+  const isAdmin = userInfo.user.is_admin;
   const userToBan = commands[0].split("|")[0].replace("<@", "");
   console.log(userToBan);
 
-  const isSelfUnshush = userToBan === user_id;
+  // const isSelfUnshush = userToBan === user_id;
 
-  if (!isSelfUnshush) {
+  if (!isAdmin) {
+    return;
+  }
+
+  if (isAdmin) {
     await client.chat.postMessage({
       channel: process.env.MIRRORCHANNEL,
       text: `<@${userToBan}> was unshushed`,
       mrkdwn: true,
     });
+
+    const updateUser = await prisma.Bans.deleteMany({
+      where: { user: userToBan },
+    });
+
+    console.log("I'm working");
+
+    await client.chat.postMessage({
+      channel: userToBan,
+      text: `You were unshushed`,
+    });
   }
-
-  const updateUser = await prisma.Bans.deleteMany({ where: { user: userToBan } });
-  console.log("I'm working");
-
-  await client.chat.postMessage({
-    channel: userToBan,
-    text: `You were unshushed`,
-  });
 }
 
 module.exports = unshush;
